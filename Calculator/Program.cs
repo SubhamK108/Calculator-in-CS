2fcb589be924b54e4ef2f9c58dc3018eb345a581
Subham Karmakar <subhamk.subs@gmail.com>
2019-11-14 15:48:05 +0530

    Added a Class

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Functions;
 
 namespace Calculator
 {
@@ -6,8 +7,15 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I am Subham Karmakar");
+            Console.WriteLine("\t\t...2 Digit Calculator...\t\t\n\n");
+            Console.WriteLine("Enter Number 1: ");
+            var n1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter Number 2: ");
+            var n2 = Convert.ToDecimal(Console.ReadLine());
+
+            var calc1 = new BasicOperations(n1, n2);
+
+            Console.WriteLine($"{n1} + {n2} = {calc1.GetAdd()}");
         }
     }
 }
